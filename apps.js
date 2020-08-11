c75c6b7050b11e8237f8fa76bdb970c03e1a2481
RazzleJazzled <robertjgassner@gmail.com>
2020-08-11 09:57:48 -0700

    added changes

--- apps.js
+++ apps.js
@@ -7,95 +7,125 @@ var startDirect = document.querySelector("#directions");
 var questionPrompt = document.querySelector("#question-prompt")
 var questionText = document.querySelector("#question-text");
 var questionOptions = document.querySelector(".question-options")
+var timerContainer = document.querySelector(".timer-container");
+var timeSpan = document.querySelector("#time");
+var questionIndex = 0;
+var time = 75;
+var timer;
 var questions = [
-    { 
-        text:"Commonly used data types do NOT include:",
+
+
+
+    {
+        text: "Commonly used data types do NOT include:",
         options: ["Strings", "Booleans", "Alerts", "Numbers"],
         answer: "Alerts",
     },
-    { 
-        text:"The condition in an if/else statement is enclosed within:",
+    {
+        text: "The condition in an if/else statement is enclosed within:",
         options: ["Quotes", "Curly Brackets", "Parenthesis", "Square Brackets"],
         answer: "Parenthesis",
     },
-    
-    { 
-        text:"Arrays in Javascript can be used to store:",
+
+    {
+        text: "Arrays in Javascript can be used to store:",
         options: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the above"],
         answer: "All of the above",
     },
-    
-    { 
-        text:"String valuse must be enclosed in ____ when being assigned to variables:",
+
+    {
+        text: "String valuse must be enclosed in ____ when being assigned to variables:",
         options: ["Commas", "Curly Brackets", "Quotation Marks", "Parenthesis"],
         answer: "Quotation Marks",
     },
-    
-    { 
-        text:"A very useful tool during devlopment and debugging for printing content to the debugger is:",
+
+    {
+        text: "A very useful tool during devlopment and debugging for printing content to the debugger is:",
         options: ["Javascript", "Terminal/Bash", "For Loops", "console.log"],
         answer: "console.log",
     }
 ]
-var questionIndex = 0;
 
-
-startGameBtn.addEventListener("click", function(e){
+startGameBtn.addEventListener("click", function (e) {
     startPrompt.style.display = "none";
     startDirect.style.display = "none";
     startGameBtn.style.display = "none";
     questionPrompt.style.display = "block";
+    timerContainer.style.display = "block";
     renderQuestion();
- 
+    startTimer();
+
 });
 
-
-
-function renderQuestion(){
-    var question =questions[questionIndex];
+function renderQuestion() {
+    var question = questions[questionIndex];
     questionText.textContent = questions[questionIndex].text;
-    console.log(questions[questionIndex].text)
     questionOptions.innerHTML = "";
-    for (var i = 0; i < question.options; i++) {
+
+    for (var i = 0; i < question.options.length; i++) {
         var btn = document.createElement("button");
-        btn.setAttribute("style", "display: initial")
-        btn.setAttribute("class", "btn btn-primary");
+        btn.setAttribute("class", "btn-primary");
         btn.setAttribute("Value", question.options[i]);
         btn.textContent = question.options[i];
         questionOptions.append(btn);
+        
     }
 
 }
 
+function startTimer() {
+
+    timer = setInterval(function () {
+        time--;
+        timeSpan.textContent = time;
+        if (time === 0) {
+            endGame();
+        }
+    }, 1000)
+}
 
 
 
-document.body.addEventListener("click", function (e){
+document.body.addEventListener("click", function (e) {
     e.preventDefault;
     if (!e.target.matches(".question-option")) return;
 
-    var value =e.target.value;
-    if (questions[questionIndex.answer] === value){
+    var value = e.target.value;
+    if (questions[questionIndex.answer] === value) {
         console.log("Correct!");
-        timer++;
+
     } else {
         console.log("You were wrong!");
-        timer--
+        time -= 10;
+        timeSpan.textContent = time;
     }
     questionIndex++;
     renderQuestion()
+
+     if (questionIndex > 3 || time === 0) {
+       endGame()
+}
 });
 
-var seconds=75;
-var timer;
-function Countdown() {
-  if(seconds < 75) {
-    document.getElementById("timer").innerHTML = seconds;
-  }
-     if (seconds >0 ) {
-         seconds--;
-     } else {
-         clearInterval(timer);
-        //kick it to the ending screen;
-     }
-} 
+endGame()
+
+
+function endGame() {
+    if (questionIndex > 3 || time === 0) {
+        clearInterval(timer);
+        questionPrompt.style.display = "none";
+        questionOptions.style.display = "none";
+       var p = document.createElement("p")
+       p.setAttribute("Style", "text-align: center");
+       p.textContent = ("Please Submit Your Score. Try to get a higher score each time. Thanks for playing!")        
+        textContent.append(p)
+        var h1 = document.createElement("h1")
+        h1.setAttribute("Style", "font-weight: bold, text-align: center");
+        h1.textContent = ("Game Over!")
+        textContent.append(h1)
+        
+
+
+
+    }
+}
